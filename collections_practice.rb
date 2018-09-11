2b65347e8309923243629ae6cd375bda0ef4951d
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-11 20:23:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,8 @@
 def begins_with_r arr
-  arr.all? {|x| x.startswith "r"}
-end
+  arr.all? {|x| x.starts_with "r"}  # ~> NoMethodError: undefined method `startswith' for "ruby":String\nDid you mean?  start_with?
+end                                # => :begins_with_r
+
+begins_with_r ["ruby", "rails", "sails"]
 
 def contain_a arr
 
@@ -29,3 +31,13 @@ end
 def organize_schools arr
 
 end
+
+# ~> NoMethodError
+# ~> undefined method `startswith' for "ruby":String
+# ~> Did you mean?  start_with?
+# ~>
+# ~> collections_practice.rb:2:in `block in begins_with_r'
+# ~> collections_practice.rb:2:in `each'
+# ~> collections_practice.rb:2:in `all?'
+# ~> collections_practice.rb:2:in `begins_with_r'
+# ~> collections_practice.rb:5:in `<main>'
