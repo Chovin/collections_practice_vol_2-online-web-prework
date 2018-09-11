624ec3474dc1dd7781266402d49c5f17c30277ce
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-11 20:57:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,8 +18,8 @@ def count_elements arr
   arr.map{|x| x.merge({count: arr.count(x)}) if not x.key?(:count)}.uniq
 end  # => :count_elements
 
-def merge_data arr, arr2
-  arr.concat arr2
+def merge_data keys, data
+  keys.map {|x| x.merge(data[0][x[:first_name]])}
 end  # => :merge_data
 
 def find_cool arr
